a35da06377cb2322f216d778f7e5583684c6e965
DataTunnel <admin@datatunnel.jp>
2019-08-17 01:35:20 +0900

    First commit.

---
+++ AudioProcessing.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+public class AudioProcessing : AssetPostprocessor
+{
+    private void OnPreprocessAudio()
+    {
+        var importer = assetImporter as AudioImporter;
+        var path = assetImporter.assetPath;
+
+        importer.forceToMono = path.Contains("SE");
+        importer.loadInBackground = path.Contains("BGM");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-unity-audio-importer
+# unity-audio-importer
+
+
+
+![import](audio-importer.jpg)
+
+![bgm](audio-importer-bgm.jpg)
+
+![se](audio-importer-se.jpg)
